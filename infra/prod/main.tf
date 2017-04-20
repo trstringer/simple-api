1fa4b819317b839a2369edd2e2606140c6e1e4b3
Thomas Stringer <tr.stringer@outlook.com>
2017-04-20 14:38:42 -0400

    add linux vm provisioning

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -1,4 +1,19 @@
-resource "azurerm_resource_group" "testrg" {
-  name     = "test-rg"
-  location = "eastus"
+variable "ssh_public_key" {
+  description = "ssh public key"
+}
+
+module "linuxvm" {
+  source = "github.com/tstringer/terraform-azure-linux-vm"
+
+  name_prefix    = "simpleapi1"
+  hostname       = "sa12345"
+  ssh_public_key = "${var.ssh_public_key}"
+}
+
+output "admin_username" {
+  value = "${module.linuxvm.admin_username}"
+}
+
+output "vm_fqdn" {
+  value = "${module.linuxvm.vm_fqdn}"
 }
